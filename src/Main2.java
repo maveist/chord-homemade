d9e2144017074c6dd008414eda5df7cbd7639c8e
Louis AUNEAU <E146329D@U-INF-J-C002-26.iut-nantes.univ-nantes.prive>
2016-02-25 12:16:48 +0100

    Ajout de la classe Peer, avec récuperation du Hash.

---
+++ src/Main2.java
@@ -0,0 +1,12 @@
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		Peer.setHashServerIp("localhost");
+		Peer.setHashServerPort(8001);
+		
+		Peer monPair = new Peer();
+
+	}
+
+}
